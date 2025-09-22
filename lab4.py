3dd323546310584c5ee18a1b6c7456cf93f712b3
Nraj414 <41475946+Nraj414@users.noreply.github.com>
2025-09-22 22:51:50 +0530

    3rd commit

--- lab4.py
+++ lab4.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "<h1> Welcome to my flask App <h1>" <br><p> This is simple flask application.</p>
+    return "<h1> Welcome to my flask App</h1><br><p> This is simple flask application.</p>"
 if  __name__== '__main__':
     app.run(debug=True)
